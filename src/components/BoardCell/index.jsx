8ea0ecbc2d49fd1c065bdcf1ddcfb09f6676c4bb
paulius-mongirdas <127019248+paulius-mongirdas@users.noreply.github.com>
2024-12-03 13:59:35 +0200

    Flyweight (#6)
    * Add new ship images
    
    * Fix image alignment in cells

--- src/components/BoardCell/index.jsx
+++ src/components/BoardCell/index.jsx
@@ -1,18 +1,18 @@
 import React, { useContext } from 'react'
 import './styles.css'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { faCircle, faX } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import GameContext from '../../GameContext'
 
-const BoardCell = ({rowIndex, colIndex, cell, handleShot, canShoot, isYourBoard}) => {
+const BoardCell = ({ rowIndex, colIndex, cell, handleShot, canShoot, isYourBoard }) => {
 
-  const {setGame} = useContext(GameContext)
-  const {id} = useParams()
+  const { setGame } = useContext(GameContext)
+  const { id } = useParams()
 
   const handleClick = (e) => {
     e.preventDefault();
-    
+
     const shotCoords = {
       row: rowIndex,
       col: colIndex,
@@ -22,25 +22,29 @@ const BoardCell = ({rowIndex, colIndex, cell, handleShot, canShoot, isYourBoard}
   };
 
   const setBgColor = () => {
-    if(cell.isShip && isYourBoard)
+    if (cell.isShip && isYourBoard)
       return 'bg-blue-500'
-    if(cell.isIsland)
+    if (cell.isIsland)
       return 'bg-amber-700'
     return 'bg-stone-300'
   }
 
   return (
     <div
-     onClick={canShoot && !cell.isIsland  ? handleClick : null}
-     className={`flex items-center justify-center block w-full h-full border border-black ${!cell.isHit && canShoot && !cell.isIsland && 'hover:bg-gray-500 cursor-pointer'} ${setBgColor()}`}
-     >
-        {(cell.isHit && !cell.isShip) && (
-          <FontAwesomeIcon icon={faCircle} />
-        )}
+      onClick={canShoot && !cell.isIsland ? handleClick : null}
+      className={`shipBox border border-black ${!cell.isHit && canShoot && !cell.isIsland && 'hover:bg-gray-500 cursor-pointer'} ${setBgColor()}`}
+    >
+      {isYourBoard && cell.isShip && !cell.isHit && (
+        <img src={`${cell.imagePath}`} width={50} height={34} alt='ship' />
+      )}
 
-       {(cell.isHit && cell.isShip) && (
-          <FontAwesomeIcon icon={faX} className='text-red-500 font-bold' />
-        )}
+      {(cell.isHit && !cell.isShip) && (
+        <FontAwesomeIcon icon={faCircle} />
+      )}
+
+      {(cell.isHit && cell.isShip) && (
+        <FontAwesomeIcon icon={faX} className='text-red-500 font-bold' />
+      )}
 
 
     </div>
